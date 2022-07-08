9ab84729f226acfc2dd6be584b4c5f712b2670df
Nguyen Tran <bryantran.dev@gmail.com>
2022-07-08 16:29:52 +0700

    init package

---
+++ src/Generate/Generate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Edric\Generate;
+
+class Generate
+{
+
+    public $lenght = 8;
+
+    public $level;
+
+    public function __construct()
+    {
+    }
+
+    public function generator()
+    {
+    }
+}
---
+++ src/GenerateProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Edric\Generate;
+
+use Illuminate\Support\ServiceProvider;
+
+class GenerateProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
