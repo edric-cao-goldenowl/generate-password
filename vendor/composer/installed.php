923ffc312532dc80bff7e85a43ba6f5e4a70aa7a
Edric Cao <edric.cao.goldenowl@gmail.com>
2022-07-08 17:09:32 +0700

    add construct method

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9ab84729f226acfc2dd6be584b4c5f712b2670df',
+        'name' => 'edric/generate',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'edric/generate' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9ab84729f226acfc2dd6be584b4c5f712b2670df',
+            'dev_requirement' => false,
+        ),
+    ),
+);
